6ceefb05e522babde97a2d8e932d6e890919f966
Mikhail <jeffgitos73@gmail.com>
2021-02-17 23:46:59 +1100

    added comments and logic

--- Python/problem1.py
+++ Python/problem1.py
@@ -1,7 +1,10 @@
 def sum_of_multiples(n):
 
+    #create a new list to hold our numbers so we can sum them later
     list_of_nums_to_sum = []
     
+    #iterates over n, if the value when divided by 3 or 5 = 0, then we add it to a list
+    #sum up everything in that list 
     for num in range(n):
         if num%3==0 or num%5==0:
             list_of_nums_to_sum.append(num)
