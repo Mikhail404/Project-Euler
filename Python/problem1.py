c41bd7789a23a8907e4e090b97194eaf30ffbd77
Mikhail <jeffgitos73@gmail.com>
2021-02-17 23:24:21 +1100

    problem 1 COMPLETE

---
+++ Python/problem1.py
@@ -0,0 +1,13 @@
+def sum_of_multiples(n):
+
+    list_of_nums_to_sum = []
+    
+    for num in range(n):
+        if num%3==0 or num%5==0:
+            list_of_nums_to_sum.append(num)
+    
+    Sum = sum(list_of_nums_to_sum)
+    print(Sum)
+    return None
+
+print(sum_of_multiples(1000))
