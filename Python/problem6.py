00b6b2d99bb841ed5e3b43154f46822f6693ffa3
Mikhail <jeffgitos73@gmail.com>
2021-02-26 15:22:02 +1100

    solved

---
+++ Python/problem6.py
@@ -0,0 +1,32 @@
+def sumOfSquares(n):
+    squares_list = []
+    numbers_list = []
+    
+    for i in range(0, n+1):
+        square = i*i
+        squares_list.append(square)
+    Sum = sum(squares_list)
+    print(Sum)
+
+
+    for i in range(0, n+1):
+        numbers_list.append(i)
+    Sum2 = sum(numbers_list)
+    squareOfSums = Sum2*Sum2
+    print(squareOfSums - Sum)
+
+
+
+
+
+sumOfSquares(100)
+
+    
+
+
+
+
+
+
+
+
